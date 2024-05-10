0078c1346efb48a131f0565afbefe4287cee7b74
pawfid <pawel.fidelus@fitech.pl>
2024-05-10 17:27:08 +0200

    generated spring boot project

---
+++ src/main/java/practical/progr/framework/tutorial/developer/FrameworkTutorialDeveloperApplication.java
@@ -0,0 +1,13 @@
+package practical.progr.framework.tutorial.developer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkTutorialDeveloperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkTutorialDeveloperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/practical/progr/framework/tutorial/developer/FrameworkTutorialDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package practical.progr.framework.tutorial.developer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkTutorialDeveloperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
